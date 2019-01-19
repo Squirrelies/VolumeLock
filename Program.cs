373ca4c00e4ac5c955491800fd6fc9a3696e73d4
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2019-01-18 22:12:27 -0600

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VolumeLock
+{
+    public static class Program
+    {
+        public const string DATE_TIME_FORMAT_STRING = "yyyy-MM-dd HH:mm:ss.fff";
+
+        public static async Task Main()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || RuntimeInformation.OSArchitecture != Architecture.X64)
+            {
+                Console.WriteLine("This program only supports Windows operating systems on the 64-bit architecture.");
+                Environment.Exit(1);
+            }
+
+            using (CancellationTokenSource cTokenSource = new CancellationTokenSource())
+            {
+                // Wait for CTRL+C so we can abort.
+                Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) => { cTokenSource.Cancel(); };
+                Console.WriteLine("[{0}] Press Ctrl+C to exit application.", DateTime.Now.ToString(Program.DATE_TIME_FORMAT_STRING));
+
+                // Create a new controller and get the default devices.
+                CoreAudioController ctrl = new CoreAudioController();
+                CoreAudioDevice playbackDevice = ctrl.DefaultPlaybackDevice;
+                CoreAudioDevice communicationsDevice = ctrl.DefaultCaptureCommunicationsDevice;
+
+                // Observe the playback device.
+                VolumeLockObserver playbackObs = new VolumeLockObserver(playbackDevice);
+                playbackObs.Subscribe(playbackDevice.VolumeChanged);
+
+                // Observe the capture device.
+                VolumeLockObserver captureObs = new VolumeLockObserver(communicationsDevice);
+                captureObs.Subscribe(communicationsDevice.VolumeChanged);
+
+                // Wait indefinitely for CTRL+C.
+                try { await Task.Delay(-1, cTokenSource.Token); }
+                catch { Environment.Exit(0); }
+            }
+        }
+    }
+}
---
+++ VolumeChangeObserver.cs
@@ -0,0 +1,51 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Diagnostics;
+
+namespace VolumeLock
+{
+    public class VolumeLockObserver : IObserver<DeviceVolumeChangedArgs>
+    {
+        private IDisposable unsubscriber;
+        private CoreAudioDevice device;
+        private readonly double lockValue;
+
+        public VolumeLockObserver(CoreAudioDevice device)
+        {
+            this.device = device;
+            this.lockValue = device.Volume;
+
+            Console.WriteLine("[{0}] Locking [{1}] volume to {2}%.", DateTime.Now.ToString(Program.DATE_TIME_FORMAT_STRING), this.device.FullName, lockValue);
+        }
+
+        public virtual void Subscribe(IObservable<DeviceVolumeChangedArgs> provider)
+        {
+            this.unsubscriber = provider.Subscribe(this);
+        }
+
+        public virtual void Unsubscribe()
+        {
+            this.unsubscriber.Dispose();
+        }
+
+        public virtual void OnCompleted()
+        {
+            Debug.WriteLine("[{0}] Additional volume changes will not be monitored or handled.", DateTime.Now.ToString(Program.DATE_TIME_FORMAT_STRING));
+        }
+
+        public virtual void OnError(Exception error)
+        {
+            Debug.WriteLine("[{0}] Exception {1}:\r\n{2}\r\n{3}", DateTime.Now.ToString(Program.DATE_TIME_FORMAT_STRING), error.GetType().ToString(), error.Message, error.StackTrace);
+        }
+
+        public virtual void OnNext(DeviceVolumeChangedArgs current)
+        {
+            if (this.lockValue != current.Volume)
+            {
+                Console.WriteLine("[{0}] Volume change on [{1}] to {2}%. Setting back to lock value of {3}%.", DateTime.Now.ToString(Program.DATE_TIME_FORMAT_STRING), this.device.FullName, current.Volume, this.lockValue);
+                this.device.Volume = this.lockValue;
+            }
+        }
+    }
+}
